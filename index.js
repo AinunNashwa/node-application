a14fe7d24d66aa94f0b2f7eb76fd3249c421430b
AinunNashwa <ainunnashwa99@gmail.com>
2022-11-03 12:16:47 +0800

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 
 const express = require('express'); //Import the express dependency
-const app = express();              //Instantiate an express app, the main work horse of this server
-const port = 5000;                  //Save the port number where your server will be listening
-
+const app = express(); 
+require("dotenv").config();
+             //Instantiate an express app, the main work horse of this server
+const port = process.env.PORT;                  //Save the port number where your server will be listening
+const api = process.env.API_URL;
 //Idiomatic expression in express to route and respond to a client request
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
     res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
